11a697fc55d36cd678b3b350a10e8ade21f7976a
Iván Yoed González Aréchiga <igonzarec@gmail.com>
2022-07-26 01:19:29 -0500

    Refactored controllers, created model and connected last ui components.

---
+++ BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Ivan Gonzalez on 26/07/22.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
